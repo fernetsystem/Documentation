a85f4d85d5a507fcf3258b31aad55b81e2fe6f6c
Jth Rdz <fernetsystem@gmail.com>
2015-09-20 18:21:38 -0500

    Model adicion de 3 funcsiones
    registrar alumno
    obtener datos del alumno
    obtener matricula

--- model/model.php
+++ model/model.php
@@ -18,6 +18,29 @@ class model{
         	return 0;
         }
 	}
+    public function get_registro_alumno($matricula,$email,$pass){
+        $consulta=$this->db->query("insert into accounts values($matricula,'$email','$pass','ALUMNO')");
+        if($consulta>0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    public function get_data_alumno($matricula,$nom,$pat,$mat,$curp,$tel){
+        $consulta=$this->db->query("insert into data_alumno values($matricula,'$nom','$pat','$mat','$curp','$tel')");
+        if($consulta>0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    public function get_matricula($matricula){
+    $consulta=$this->db->query("select * from accounts where matricula=".$matricula."");
+        while ($filas=$consulta->fetch_assoc()) {
+            $this->users[]=$filas;
+        }
+        return $this->users;
+    }
 }
 	
 ?>
