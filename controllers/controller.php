04290055a2465d4855be09c6427f50483d0723d1
Jth Rdz <fernetsystem@gmail.com>
2015-09-20 06:26:28 -0500

    Agregar métodos para ver vistas en el nav

--- controllers/controller.php
+++ controllers/controller.php
@@ -7,31 +7,55 @@ class controller{
 		$this->myModel = new model();
 	}
 	public function Index(){
-        require_once 'header.inc';
+        require_once 'views/header.inc';
 		require_once 'views/loginUser.php';
-        #require_once 'views/registrarUser.php';
-        require_once 'footer.inc';
+        require_once 'views/footer.inc';
 	}
 
 	public function valida(){
 		$datos = $this->myModel->get_login($_REQUEST['mat'],$_REQUEST['pass']);
 		if($datos > 0){
-			echo "SUCCESS";
+			$getData=$this->myModel->get_matricula($_REQUEST['mat']);
+			require_once 'views/headerAlumno.inc';
+			require_once 'views/homeAlumno.php';
+			require_once 'views/footerAlumno.inc';
 		}else{
 			echo "ERROR";
 		}
 	}
+	public function registraAlumno(){
+		$datos = $this->myModel->get_registro_alumno($_REQUEST['mat'],$_REQUEST['email'],$_REQUEST['pass']);
+		if($datos > 0){
+			echo "SUCCESS";
+		}else{
+			echo "ERROR";
+		}
+		$myController = new controller();
+		$myController->formAddLogin();
+	}
 	#Mandar a llamar el formulario para registrar alumno
 	public function formAddAlum(){
-        require_once 'header.inc';
+        require_once 'views/header.inc';
 		require_once 'views/registrarUser.php';
-        require_once 'footer.inc';
+        require_once 'views/footer.inc';
 	}
 	#Mandar a llamar el formulario para logearce
 	public function formAddLogin(){
-        require_once 'header.inc';
+        require_once 'views/header.inc';
 		require_once 'views/loginUser.php';
-        require_once 'footer.inc';
+        require_once 'views/footer.inc';
+	}
+	#Mandar a llamar el formulario de los datos del alumno
+	public function dataAlumno(){
+		require_once 'views/headerAlumno.inc';
+		require_once 'views/fillDataAlumno.php';
+        require_once 'views/footerAlumno.inc';
+	}
+	#Mandar a llamar el formulario de todos los documentos
+	public function formDocuments(){
+		require_once 'views/headerAlumno.inc';
+		require_once 'views/allDocuments.php';
+        require_once 'views/footerAlumno.inc';
 	}
 
 }
