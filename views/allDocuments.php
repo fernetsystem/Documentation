d9107ae56d1edbe1781743492b0960eadb4882c4
Jth Rdz <fernetsystem@gmail.com>
2015-09-20 06:22:20 -0500

    Lista para administrar documentos

---
+++ views/allDocuments.php
@@ -0,0 +1,34 @@
+<h3>Mis Documentos</h3>
+<table class="highlight responsive-table">
+        <thead>
+          <tr>
+              <th data-field="Documento">DOCUMENTO</th>
+              <th data-field="Formato">FORMATO</th>
+              <th data-field="Estado">ESTADO</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Carta compromiso</td>
+            <td>205BK20000/02 (F3)</td>
+            <td>Sin llenar</td>
+          </tr>
+          <tr>
+            <td>Carta de Presentación</td>
+            <td>205BK20000/02 (F2)</td>
+            <td>Sin llenar</td>
+          </tr>
+          <tr>
+            <td>Carta de Aceptación</td>
+            <td>205BK20000/02 (F4) </td>
+            <td>Sin llenar</td>
+          </tr>
+		  <tr>
+            <td>Proyecto de Cooperación</td>
+            <td>205BK20000/02(F01)</td>
+            <td>Sin llenar</td>
+          </tr>
+          
+        </tbody>
+</table>
