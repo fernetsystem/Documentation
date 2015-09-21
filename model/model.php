72f8dd75d9ba9e4779184ee59bab25d3f08fa142
Jth Rdz <fernetsystem@gmail.com>
2015-09-21 03:34:13 -0500

    Add Método para listado de documentos en estancias 1

--- model/model.php
+++ model/model.php
@@ -34,12 +34,27 @@ class model{
             return 0;
         }
     }
+    /*public function fill_table_proceso($matricula,$carrera){//implementar trigger
+        $consulta=$this->db->query("insert into procesos values($matricula,'Estancias 1','No aprovado','4° Cuatrimeste','Primer ciclo de formacion','---',null,null,$carrera,'---','---',null)");
+        if($consulta>0){
+            return 1;
+        }else{
+            return 0;
+        }   
+    }*/
     public function get_matricula($matricula){
     $consulta=$this->db->query("select * from accounts where matricula=".$matricula."");
         while ($filas=$consulta->fetch_assoc()) {
             $this->users[]=$filas;
         }
         return $this->users;
+    }
+    public function fill_documents_estancia1($matricula){
+        $consulta=$this->db->query("select documento,formato,estado from procesos_documentos where matricula=$matricula and proceso='Estancias1'");
+        while ($filas=$consulta->fetch_assoc()) {
+            $this->users[]=$filas;
+        }
+        return $this->users;   
     }
 }
 	
