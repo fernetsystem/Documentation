43d695683fefef13cca92edba8af270cda65d1c9
Jth Rdz <fernetsystem@gmail.com>
2015-09-26 00:51:30 -0500

    Agregar carreras en formulario de datos alumnos

--- views/fillDataAlumno.php
+++ views/fillDataAlumno.php
@@ -3,7 +3,7 @@
 <div class="row">
 	<div class="col s2"></div>
 	<div class="col s8">
-		<form action="index.php?c=controller&a=fillDataAlumno&mat=mat&nom=nom&pat=pat&mate=mate&crp=crp&te=tel; ?>" method="post">
+		<form action="index.php?c=controller&a=fillDataAlumno&mat=mat&nom=nom&pat=pat&mate=mate&crp=crp&te=tel&; ?>" method="post">
 			<div class="row">
 				<div class="input-field col s6">
 					<input disabled value=<?php echo $_SESSION['matr']; ?> type="text" id="mat"name="mat">
@@ -33,6 +33,17 @@
 					<input type="text" id="tel"name="tel">
 					<label for="tel">Telefono:</label>
 				</div>
+				
+			</div>
+			<div class="row">	
+				<select name="carr"class="s6">
+			      <option value="" disabled selected>Selecciona tu carrera</option>
+			      <option value="1">Ingenieria en informatica</option>
+			      <option value="2">Ingenieria en mecatronica</option>
+			      <option value="3">Ingenieria en nanotecnologia</option>
+			      <option value="3">Ingenieria industrial</option>
+			      <option value="3">Licenciatura en admonistracion</option>
+			    </select>
 			</div>
 			<button class="btn waves-effect waves-light" type="submit">Aceptar</button>
 		</form>
