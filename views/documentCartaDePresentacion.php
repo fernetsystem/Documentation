43ee706f837503941c884e8fcbb80853203e1072
Jth Rdz <fernetsystem@gmail.com>
2015-09-29 16:09:20 -0500

    Formatos para los procesos

---
+++ views/documentCartaDePresentacion.php
@@ -0,0 +1,38 @@
+<div class="row">
+      <div class="col s12 m8 l8 offset-l2 offset-m2" style="float:center;">
+        <div class="card-panel grey lighten-5">
+          <span class="black-text">
+<p> (PAPEL MEMBRETADO)</p>
+             
+<p>(MunicipIo), Estado de México a 00 de xxx de 201x</p>
+ 
+ 
+<p>DR. JULIO CESAR RUÍZ MARTÍNEZ</p>
+<p>DIRECTOR DE LA DIVISIÓN DE INGENIERÍA EN INFORMÁTICA</p>
+<p>UNIVERSIDAD POLITÉCNICA DEL VALLE DE MÉXICO</p>
+<p>PRESENTE</p>
+ 
+<p>Asunto: Carta de Aceptación del alumno(a)</p>
+<p> Por medio de la presente me permito informarle que el alumno(a) (Nombre completo del alumno(a), 
+	con el siguiente orden: Apellido paterno, apellido materno y nombre(s). Este dato se anotará en Negrita) 
+	de la carrera de Ingeniería en Informática ha sido aceptado(a) para realizar su (Estancia I ó Estancia II) en esta organización, 
+	adscrito al Nombre del área de la empresa en la que participará el alumno(a)   desarrollando el proyecto Nombre completo del proyecto, 
+	con base en el Formato de definición del Proyecto de Cooperación. durante el periodo del  Anotar el rango de la fecha en las que el alumno 
+	realizará su estancia con 120 horas.
+
+<p>Así mismo le presento al (nombre del asesor externo) (correo del asesor externo) como Asesor Externo  quien tendrá como responsabilidad supervisar, 
+	asesorar y evaluar el proyecto del alumno en colaboración con el Profesor Responsable de la Universidad.</p>
+<p>Para los trámites a que haya lugar y sin otro particular por el momento, aprovecho la ocasión para enviarle un cordial saludo</p>
+ 
+<p>ATENTAMENTE</p>
+ 
+ 
+<p>«nombre del responsable de la estancia»</p>
+<p>«puesto»</p>
+<p>Sello de la empresa</p>
+
+
+            </span>
+        </div>
+      </div>
+    </div>
