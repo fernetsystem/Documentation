4036b7044dd4a79dd57c28418274fe18e6d1fc52
Jth Rdz <fernetsystem@gmail.com>
2015-10-08 02:26:29 -0500

    Acomodar Responsive login

--- views/loginUser.php
+++ views/loginUser.php
@@ -1,13 +1,30 @@
 <div class="row">
-    <div class="col s12 m4 l8">
+    <div class="col s12 m16 l6 offset-s1 offset-m3 offset-l4">
     	<h3>Iniciar sesion</h3>
 		<form action="index.php?c=controller&a=valida&mat=mat&pass=pass; ?>" method="post">
-			<input type="text" name="mat" placeholder="Matricula"/>	<br/><br/>
-			<input type="password" name="pass" placeholder="Contraseña" />	<br/><br/>
-			<!--<input type="submit" class="waves-effect waves-light btn" value="Ingresar"/>-->
-			<button class="btn waves-effect waves-light" type="submit">Ingresar
-		    <!--<i class="material-icons right">send</i>-->
-		  </button>
+			<div class="row">
+			<div class="input-field col s10 m6 l6">
+				<input type="text" id="mat"name="mat">
+				<label for="mat">Matricula:</label>
+			</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s10 m6 l6">
+						<input type="text" id="pass"name="pass">
+						<label for="pass">Contraseña:</label>
+				</div>
+			</div>
+			<button class="btn waves-effect waves-light" type="submit">Ingresar</button>
 		</form>
     </div>
-  </div>
+</div>
+<div class="row">
+	<div class="col s12 m16 l6 offset-s1 offset-m3 offset-l4">
+		<div class="col l2 m2 s12">
+			<a href="index.php?c=controller&a=formAddAlum&;?>">Registrate</a>
+		</div>
+		<div class="col l4 m4 s12 offset-m1 offset-l1 "> 
+			<a href="index.php?c=controller&a=formRecoverPass&;?>">Recuperar contraseña</a>
+		</div>
+	</div>
+</div>
