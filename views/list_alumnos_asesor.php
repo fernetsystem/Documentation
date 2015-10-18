1ba5dc890275a99e46f05811e70f3dadd31a850a
Jth Rdz <fernetsystem@gmail.com>
2015-10-18 04:44:36 -0500

    Pre vista de listado de alumnos para cada grupo

---
+++ views/list_alumnos_asesor.php
@@ -0,0 +1,20 @@
+<table class="highlight responsive-table">
+        <thead>
+          <tr>
+              <th data-field="Matricula">Matricula</th>
+              <th data-field="Nombre">Nombre</th>
+              <th data-field="Paterno">Paterno</th>
+              <th data-field="Estado">Estado</th>
+          </tr>
+        </thead>
+        <tbody>
+        	<?php #foreach ($datos as $data) {
+        		echo "<tr>";
+	        		echo "<td>xxxx</td>";
+	        		echo "<td>cfretrgte</td>";
+              echo "<td>gegetgeeg</td>";
+              echo "<td>Activado</td>";
+     		    echo "</tr>";
+        	##} ?>
+        </tbody>
+</table>
