85396c873be79cc1b0ac49cdd84e179213819122
Jth Rdz <fernetsystem@gmail.com>
2015-10-18 04:39:45 -0500

    Prevista de grupos para asesor

---
+++ views/myGroups.php
@@ -0,0 +1,47 @@
+
+<div class="row">
+    <div class="col s12 m10 l6 offset-s1 offset-m1 offset-l3">
+    	<h2 class="center-align">Grupos</h2>	
+      <ul class="tabs">
+        <li class="tab col s2"><a class="active" href="#test1">ESTACIAS1</a></li>
+        <li class="tab col s2"><a href="#test2">ESTANCIAS2</a></li>
+        <li class="tab col s2"><a href="#test3">ESTADIAS</a></li>
+      </ul>
+    </div>
+</div>
+<div id="test1" class="col s12">	
+	<div class="row" >
+		<div class="col s12 m10 l6 offset-s1 offset-m1 offset-l3">
+			<ul class="collection with-header">
+		        <li class="collection-header"><h4>Periodo</h4></li>
+		        <li class="collection-item"><div>2015-3<a href="index.php?c=controller&a=groupSelect&; ?" class="secondary-content"><i class="material-icons">ver</i></a></div></li>
+		        <li class="collection-item"><div>2015-2<a href="index.php?c=controller&a=groupSelect&; ?" class="secondary-content"><i class="material-icons">ver</i></a></div></li>
+		        <li class="collection-item"><div>2015-1<a href="index.php?c=controller&a=groupSelect&; ?" class="secondary-content"><i class="material-icons">ver</i></a></div></li>
+		        <li class="collection-item"><div>2014-3<a href="index.php?c=controller&a=groupSelect&; ?" class="secondary-content"><i class="material-icons">ver</i></a></div></li>
+	      	</ul>
+		</div>
+	</div>
+</div>
+<div id="test2" class="col s12">	
+	<div class="row" >
+		<div class="col s12 m10 l6 offset-s1 offset-m1 offset-l3">
+			<ul class="collection with-header">
+		        <li class="collection-header"><h4>Periodo</h4></li>
+		        <li class="collection-item"><div>2015-3<a href="index.php?c=controller&a=groupSelect&; ?" class="secondary-content"><i class="material-icons">ver</i></a></div></li>
+		        <li class="collection-item"><div>2015-2<a href="index.php?c=controller&a=groupSelect&; ?" class="secondary-content"><i class="material-icons">ver</i></a></div></li>
+		        <li class="collection-item"><div>2015-1<a href="index.php?c=controller&a=groupSelect&; ?" class="secondary-content"><i class="material-icons">ver</i></a></div></li>
+	      	</ul>
+		</div>
+	</div>
+</div>
+<div id="test3" class="col s12">	
+	<div class="row" >
+		<div class="col s12 m10 l6 offset-s1 offset-m1 offset-l3">
+			<ul class="collection with-header">
+		        <li class="collection-header"><h4>Periodo</h4></li>
+		        <li class="collection-item"><div>2015-3<a href="index.php?c=controller&a=groupSelect&; ?" class="secondary-content"><i class="material-icons">ver</i></a></div></li>
+		        <li class="collection-item"><div>2015-2<a href="index.php?c=controller&a=groupSelect&; ?" class="secondary-content"><i class="material-icons">ver</i></a></div></li>
+	      	</ul>
+		</div>
+	</div>
+</div>
