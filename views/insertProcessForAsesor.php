4f8e7e74862dd18ce590b4366dc7ffdf8e350c48
Jth Rdz <fernetsystem@gmail.com>
2015-10-18 04:44:11 -0500

    Agregar alumno al periodo y "grupo"

---
+++ views/insertProcessForAsesor.php
@@ -0,0 +1,16 @@
+<h3>Lista de alumnos</h3>
+<div class="row" >
+	<div class="col s12 m10 l6 offset-s1 offset-m1 offset-l3">
+		<form action="index.php?c=controller&a=addTokenAsesor&matr=matr; ?>" method="post">
+			<div class="row">
+				<div class="input-field col s10 m6 l6">
+					<input type="text" id="matr"name="matr">
+					<label for="matr">Matricula del alumno:</label>
+				</div>
+			</div>
+			<div class="row ">
+			<button class="btn waves-effect waves-light col s10 m4 l4 " type="submit">Agregar al grupo</button><br/><br/>
+			</div>
+		</form>
+	</div>
+</div>
