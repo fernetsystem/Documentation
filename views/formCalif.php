49fc0c7bf0d5261d1ba2e806aa960a63a70ac846
Jth Rdz <fernetsystem@gmail.com>
2015-10-28 05:40:45 -0600

    Corregir no muestra valores el rango

--- views/formCalif.php
+++ views/formCalif.php
@@ -2,3 +2,13 @@
 					<input type="hidden" id="mat"name="mat" value=<?php echo $matriculaDelAlumno; ?>>
 				</div>
 			</div>
+	
+			<div class="row">
+				<div class="range_field">
+			 		<input type="range" name="calf" min="0" max="10" class="col s10 m6 l6"/>
+			 	</div>
+			</div>
+			<button class="btn waves-effect waves-light" type="submit">Aceptar</button><br/><br/>
+		</form>
+	</div>
+</div>
