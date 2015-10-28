ff8e95ec4f11c1440c62eee61e143366b095980d
Jth Rdz <fernetsystem@gmail.com>
2015-10-28 05:41:21 -0600

    Mostrar matricula y nombre del alumno a evaluar en parrafos

--- views/formCalif.php
+++ views/formCalif.php
@@ -1,3 +1,13 @@
+<div class="row" >
+	<div class="col s12 m10 l6 offset-s1 offset-m3 offset-l5">
+		<br/><h4>Calificar</h4>
+		<form action="index.php?c=controller&a=updateProcE1Calfc&mat=mat&calf=calf; ?>" method="post">
+		
+			<div class="row">
+				<div class="input-field col s10 m10 l6">
+					<p>Matricula: <?php echo $matriculaDelAlumno; ?></p>
+					<p>Nombre: <?php echo $nombreDelAlumno; ?></p>
+				</div>
 				<div class="input-field col s10 m6 l6">
 					<input type="hidden" id="mat"name="mat" value=<?php echo $matriculaDelAlumno; ?>>
 				</div>
