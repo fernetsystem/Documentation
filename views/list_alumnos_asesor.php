b6550f6263cfc2ea8fafce3196ba4f0b8785bf59
Jth Rdz <fernetsystem@gmail.com>
2015-10-28 05:47:03 -0600

    Mandar las variables para calificar

--- views/list_alumnos_asesor.php
+++ views/list_alumnos_asesor.php
@@ -8,7 +8,7 @@
               <th data-field="Paterno">Paterno</th>
               <th data-field="Materno">Materno</th>
               <th data-field="Estado">Estado</th>
-              <!--#<th data-field="Total Horas">Total Horas</th>-->
+              <!--<th data-field="Total Horas">idmateria</th>-->
               <th data-field="RFC">RFC</th>
               <th data-field="INICIO">Inicio</th>
               <th data-field="TERMINO">Termino</th>
@@ -24,12 +24,15 @@
               echo "<td>".$data['paterno']."</td>";
               echo "<td>".$data['materno']."</td>";
               echo "<td>".$data['estado_pro']."</td>";
-              #echo "<td>".$data['total_horas']."</td>";
+              #echo "<td>".$data['idmateria']."</td>";
               echo "<td>".$data['RFC']."</td>";
               echo "<td>".$data['fecha_i']."</td>";
               echo "<td>".$data['fecha_t']."</td>";
               echo "<td>".$data['calificacion']."</td>";
               echo "<td>".$data['nom_proyecto']."</td>";
+              echo "<td><a href='index.php?c=controller&a=e&rfc=rfc&' class='waves-effect waves-light btn-flat teal'><font color='white'>Validar</font></a></td>";
+              echo "<td><a href='index.php?c=controller&a=formCalif&mat=".$data['matricula']."&nom=".$data['nombre']."&pater=".$data['paterno']."&mater=".$data['materno']."' class='waves-effect waves-light btn-flat amber darken-4'><font color='white'>Calificar</font></a></td>";
+              #echo "<td><a href='index.php?c=controller&a=deleteSuggesEnterprise&rfc=".$data['RFC']."' class='waves-effect waves-light btn red'>Validar</a></td>";
      		    echo "</tr>";
    
    #nom_encargado
