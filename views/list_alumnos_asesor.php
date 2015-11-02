4f3e02722929e41273752b2451a1172ffee43952
Jth Rdz <fernetsystem@gmail.com>
2015-11-02 03:39:50 -0600

    Mostar menos datos de la lista del grupo

--- views/list_alumnos_asesor.php
+++ views/list_alumnos_asesor.php
@@ -8,12 +8,13 @@
               <th data-field="Paterno">Paterno</th>
               <th data-field="Materno">Materno</th>
               <th data-field="Estado">Estado</th>
-              <!--<th data-field="Total Horas">idmateria</th>-->
               <th data-field="RFC">RFC</th>
               <th data-field="INICIO">Inicio</th>
               <th data-field="TERMINO">Termino</th>
               <th data-field="Calificación">Calificación</th>
-              <th data-field="Nom. Proyecto">Nom. Proyecto</th>
+              <!--<th data-field="Nom. Proyecto">Nom. Proyecto</th>
+              <th data-field="Nom. Enc">Nom. Encargado</th>
+              <th data-field="Puesto del enc.">Nom. Proyecto</th>-->
           </tr>
         </thead>
         <tbody>
@@ -24,13 +25,14 @@
               echo "<td>".$data['paterno']."</td>";
               echo "<td>".$data['materno']."</td>";
               echo "<td>".$data['estado_pro']."</td>";
-              #echo "<td>".$data['idmateria']."</td>";
               echo "<td>".$data['RFC']."</td>";
               echo "<td>".$data['fecha_i']."</td>";
               echo "<td>".$data['fecha_t']."</td>";
               echo "<td>".$data['calificacion']."</td>";
-              echo "<td>".$data['nom_proyecto']."</td>";
-              echo "<td><a href='index.php?c=controller&a=e&rfc=rfc&' class='waves-effect waves-light btn-flat teal'><font color='white'>Validar</font></a></td>";
+              #echo "<td>".$data['nom_proyecto']."</td>";
+              #echo "<td>".$data['nom_encargado']."</td>";
+              #echo "<td>".$data['puesto_encargado']."</td>";
+              echo "<td><a href='index.php?c=controller&a=viewDataProcess&mat=".$data['matricula']."' class='waves-effect waves-light btn-flat teal'><font color='white'>Ver</font></a></td>";
               echo "<td><a href='index.php?c=controller&a=formCalif&mat=".$data['matricula']."&nom=".$data['nombre']."&pater=".$data['paterno']."&mater=".$data['materno']."' class='waves-effect waves-light btn-flat amber darken-4'><font color='white'>Calificar</font></a></td>";
               #echo "<td><a href='index.php?c=controller&a=deleteSuggesEnterprise&rfc=".$data['RFC']."' class='waves-effect waves-light btn red'>Validar</a></td>";
      		    echo "</tr>";
