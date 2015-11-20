caf1b48e1696e2a6dacdb755b2e7f945732636bc
Jth Rdz <fernetsystem@gmail.com>
2015-11-20 06:45:07 -0600

    Documentar codigo controlador

--- controllers/controller.php
+++ controllers/controller.php
@@ -52,7 +52,7 @@ class controller{
             echo "ERROR";
         }
 	}
-	public function editData(){#probar editanto matricula
+	public function editData(){#EDITAR LOS DATOS
 		session_start();
 		$datos = $this->myModel->edit_data($_SESSION['matr'],$_REQUEST['mat'],$_REQUEST['pass'],$_REQUEST['nom'],$_REQUEST['pat'],$_REQUEST['mate'],$_REQUEST['email'],$_REQUEST['crp'],$_REQUEST['sex']);
 		if($datos > 0){
@@ -87,7 +87,8 @@ class controller{
         require_once 'views/myPassRecover.php';        
         require_once 'views/footer.inc';
 	}
-	public function recoverPass(){
+	#Recupera contraseña y envia correo
+	public function recoverPass(){ 
 		$subject = "Sistema estancias y estadias RECOVER PASS";
 		$p="";
 		$destino = $_REQUEST['emai'];
@@ -148,7 +149,7 @@ class controller{
 		$datos = $this->myModel->fill_documents($_SESSION['matr'],3);
 		require_once 'views/allDocuments.php';        
 	}
-	
+	#Muestra formulario-documentos-proceso no habilitado
 	public function fillDataEnterpriseEs1(){
 		session_start();
 		$validaMateria="";
@@ -173,7 +174,7 @@ class controller{
 
 		}
 	}
-	
+	#Muestra formulario-documentos-proceso no habilitado
 	public function fillDataEnterpriseEs2(){
 		session_start();
 		$validaMateria="";
@@ -198,6 +199,7 @@ class controller{
 
 		}
 	}
+	#Muestra formulario-documentos-proceso no habilitado
 	public function fillDataEnterpriseEstadias(){
 		session_start();
 		$validaMateria="";
@@ -222,6 +224,7 @@ class controller{
 
 		}
 	}
+	#Al llenar el formulario de su proceso se actualiza su proceso para que lo valide el asesor
 	public function updateProceesE1(){
 		session_start();
 		$datos = $this->myModel->update_data_process($_REQUEST['rfc'],$_REQUEST['fi'],$_REQUEST['ft'],$_REQUEST['pro'],$_REQUEST['nomc'],$_REQUEST['pue'],$_REQUEST['tit'],$_SESSION['matr'],1);
@@ -231,6 +234,7 @@ class controller{
 			echo "ERROR";
 		}
 	}
+	#Al llenar el formulario de su proceso se actualiza su proceso para que lo valide el asesor
 	public function updateProceesE2(){
 		session_start();
 		$datos = $this->myModel->update_data_process($_REQUEST['rfc'],$_REQUEST['fi'],$_REQUEST['ft'],$_REQUEST['pro'],$_REQUEST['nomc'],$_REQUEST['pue'],$_REQUEST['tit'],$_SESSION['matr'],2);
@@ -240,6 +244,7 @@ class controller{
 			echo "ERROR";
 		}
 	}
+	#Al llenar el formulario de su proceso se actualiza su proceso para que lo valide el asesor
 	public function updateProceesEstad(){
 		session_start();
 		$datos = $this->myModel->update_data_process($_REQUEST['rfc'],$_REQUEST['fi'],$_REQUEST['ft'],$_REQUEST['pro'],$_REQUEST['nomc'],$_REQUEST['pue'],$_REQUEST['tit'],$_SESSION['matr'],3);
@@ -249,13 +254,13 @@ class controller{
 			echo "ERROR";
 		}
 	}
-	#Mandar a llamar el formulario para sugerir empresa - Alumno
+	#Mandar a llamar el formulario para sugerir empresa
 	public function suggestEnterprise(){
 		require_once 'views/headerAlumno.inc';		
 		require_once 'views/suggest_enterprise.php';        
 		require_once 'views/footerAlumno.inc';
 	}
-	#Enviar sugerencia de empresa - Alumno
+	#Enviar sugerencia de empresa
 	public function sendSuggestEnterprise(){
 		$datos = $this->myModel->suggest_enterprise($_REQUEST['rfc'],$_REQUEST['nom_emp'],$_REQUEST['sec'],$_REQUEST['dir'],$_REQUEST['tel'],$_REQUEST['email']);
 		if($datos > 0){
@@ -264,7 +269,7 @@ class controller{
 			echo "ERROR";
 		}
 	}
-	#Obtener id del documento
+	#Obtener id del documento y mostrar PDFS
 	public function printNdoc(){
 				$datos = $this->myModel->search_document($_REQUEST['no_documento']);
 		$documentToView=$_REQUEST['doc'];
@@ -299,7 +304,7 @@ class controller{
 					#############################################
 					#################ASESOR######################
 					#############################################
-	public function registraAsesor(){
+	public function registraAsesor(){ #Registra el asesor
 		$exitsToken = $this->myModel->validate_token($_REQUEST['token'],$_REQUEST['mat']);
 		if ($exitsToken>0) {
 			$datos = $this->myModel->get_registro_asesor($_REQUEST['mat'],$_REQUEST['pass'],$_REQUEST['nom'],$_REQUEST['pat'],$_REQUEST['mate'],$_REQUEST['email'],$_REQUEST['crp'],$_REQUEST['carr'],$_REQUEST['sex']);
@@ -315,13 +320,15 @@ class controller{
 		}
 
 	}
-	public function listEnterprises(){ #View Asesor
+	#listado de empresas
+	public function listEnterprises(){ 
 		$datos = $this->myModel->get_list_enterprises();
 		require_once 'views/headerAsesor.inc';				
 		require_once 'views/list_enterprises.php'; 
 		require_once 'views/footerAsesor.inc';            		
 
 	}
+	#listado de empresas sugeridas
 	public function listEnterprisesSuggest(){ #View Asesor
 		$datos = $this->myModel->get_list_enterprises_suggest();
 		require_once 'views/headerAsesor.inc';				
@@ -329,6 +336,7 @@ class controller{
 		require_once 'views/footerAsesor.inc';            		
 
 	}
+	#dar de alta la empresa
 	public function updateEstateEnterpriseAlta(){ #Cambiar el estado a 1 que es alta de empresa
 		$datos = $this->myModel->alta_enterprise_asesor($_REQUEST['rfc']);
 		if($datos > 0){
@@ -337,6 +345,7 @@ class controller{
 			echo "ERROR";
 		}
 	}
+	#eliminar empresa sugerida
 	public function deleteSuggesEnterprise(){
 		$datos = $this->myModel->delete_enterprise_suggest($_REQUEST['rfc']);
 		if($datos > 0){
@@ -345,6 +354,7 @@ class controller{
 			echo "ERROR";
 		}	
 	}
+	#listado de grupos para el asesor en estancias 1
 	public function myListGroupsE1(){
 		session_start();
 		$datos = $this->myModel->all_my_groups_E1($_SESSION['numAsesor']);
@@ -352,6 +362,7 @@ class controller{
 		require_once 'views/myGroupsE1.php'; 
 		require_once 'views/footerAsesor.inc';
 	}
+	#listado de grupos para el asesor en estancias 2
 	public function myListGroupsE2(){
 		session_start();
 		$datos = $this->myModel->all_my_groups_E2($_SESSION['numAsesor']);
@@ -359,6 +370,7 @@ class controller{
 		require_once 'views/myGroupsE2.php'; 
 		require_once 'views/footerAsesor.inc';	
 	}
+	#listado de grupos para el asesor en estancias 3
 	public function myListGroupsEstad(){
 		session_start();
 		$datos = $this->myModel->all_my_groups_Estad($_SESSION['numAsesor']);
@@ -366,6 +378,7 @@ class controller{
 		require_once 'views/myGroupsEstad.php'; 
 		require_once 'views/footerAsesor.inc';	
 	}
+	#ver alumnos del grupo seleccionado
 	public function groupSelect(){
 		session_start(); //guardar el idgrupo seleccinado y la idmateria
 		$_SESSION['idgroup'] = $_REQUEST['idgroup'];
@@ -398,10 +411,9 @@ class controller{
 		require_once 'views/formAddGroup.php'; 
 		require_once 'views/footerAsesor.inc';	
 	}
+	#agregar nuevo grupo
 	public function addGroupAsesor(){
 		session_start();
-		//________VALIDAR QUE NO SE DUPLIQUEN GRUPOS
-		
 		$datos = $this->myModel->add_group_asesor($_REQUEST['group'],$_REQUEST['per'],$_SESSION['numAsesor']);
 		if($datos > 0){
 			echo "SUCCESS";
@@ -409,6 +421,7 @@ class controller{
 			echo "ERROR";
 		}
 	}
+	#ver proceso del alumno
 	public function viewDataProcess(){
 		session_start();
 		$datos = $this->myModel->get_all_data_alumno_process($_SESSION['idmater'],$_REQUEST['mat']);
@@ -416,6 +429,7 @@ class controller{
 		require_once 'views/viewDataProcess.php'; 
 		require_once 'views/footerAsesor.inc';
 	}
+	#generar documentos al validar
 	public function generateDocuments(){
 		session_start();
 		$datos = $this->myModel->generate_documents($_SESSION['idmater'],$_REQUEST['proc']);
@@ -425,6 +439,7 @@ class controller{
 			echo "ERROR";
 		}
 	}
+	#formulario para calificar
 	public function formCalif(){
 		$matriculaDelAlumno = $_REQUEST['mat']; #variable oculta para reutilizarla
 		$nomb = $_REQUEST['nom'];
@@ -435,6 +450,7 @@ class controller{
 		require_once 'views/formCalif.php'; 
 		require_once 'views/footerAsesor.inc';  
 	}
+	#Realizar una update en su procesos
 	public function updateProcE1Calfc(){
 		session_start();	//La session para obtener el id de la materia
 		if ($_REQUEST['calf']>=7) { 
@@ -460,6 +476,7 @@ class controller{
 						############################################
 						#################ADMIN######################
 						############################################
+	#formulario para adminitrar tokens
 	public function formTokensAdmin(){
 		require_once 'views/headerAdm.inc';				
 		require_once 'views/formTokensAdm.php'; 
@@ -467,6 +484,7 @@ class controller{
 		$myController->allTokens();	
 		require_once 'views/footerAdm.inc';            				
 	}
+	#agregar un token
 	public function addTokenAsesor(){
 		$datos = $this->myModel->add_token_asesor($_REQUEST['token'],$_REQUEST['matr']);
 		if($datos > 0){
@@ -475,6 +493,7 @@ class controller{
 			echo "ERROR";
 		}
 	}
+	#mostrar todos los tokens y asesores
 	public function allTokens(){
 		$datos = $this->myModel->all_tokens();
 				
